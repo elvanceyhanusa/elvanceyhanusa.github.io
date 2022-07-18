2b5ef6e63c27661b41e3d631618fcb0cf60e90ef
Libo Sun <sunlibo@yahoo.com>
2022-07-17 22:58:20 -0400

    updates

--- src/components/Template/SideBar.js
+++ src/components/Template/SideBar.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import ContactIcons from '../Contact/ContactIcons';
+import ContactIcons from "../Contact/ContactIcons";
 
 const { PUBLIC_URL } = process.env; // set automatically from package.json:homepage
 
@@ -12,32 +12,42 @@ const SideBar = () => (
         <img src={`${PUBLIC_URL}/images/me.jpg`} alt="" />
       </Link>
       <header>
-        <h2>Michael D&apos;Angelo</h2>
-        <p><a href="mailto:michael.l.dangelo@gmail.com">michael.l.dangelo@gmail.com</a></p>
+        <h2>Elvan Ceyhan</h2>
+        <p>
+          <a href="mailto:ceyhan@auburn.edu ">ceyhan@auburn.edu</a>
+        </p>
       </header>
     </section>
 
     <section className="blurb">
       <h2>About</h2>
-      <p>Hi, I&apos;m Michael. I like building things.
-        I am a <a href="https://icme.stanford.edu/">Stanford ICME</a> graduate, YC Alumni, and
-        the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I was
-        at <a href="https://matroid.com">Matroid</a>
-        , <a href="https://planet.com">Planet</a>
-        , <a href="https://planetaryresources.com">Planetary Resources</a>
-        , <a href="https://facebook.com">Facebook</a>
-        , and <a href="https://seds.org">SEDS</a>.
+      <p>
+        Hi, I&apos;m Michael. I like building things. I am a <a href="https://icme.stanford.edu/">Stanford ICME</a>{" "}
+        graduate, YC Alumni, and the co-founder and CTO of <a href="https://arthena.com">Arthena</a>. Before Arthena I
+        was at <a href="https://matroid.com">Matroid</a>, <a href="https://planet.com">Planet</a>,{" "}
+        <a href="https://planetaryresources.com">Planetary Resources</a>, <a href="https://facebook.com">Facebook</a>,
+        and <a href="https://seds.org">SEDS</a>.
       </p>
       <ul className="actions">
         <li>
-          {!window.location.pathname.includes('/resume') ? <Link to="/resume" className="button">Learn More</Link> : <Link to="/about" className="button">About Me</Link>}
+          {!window.location.pathname.includes("/resume") ? (
+            <Link to="/resume" className="button">
+              Learn More
+            </Link>
+          ) : (
+            <Link to="/about" className="button">
+              About Me
+            </Link>
+          )}
         </li>
       </ul>
     </section>
 
     <section id="footer">
       <ContactIcons />
-      <p className="copyright">&copy; Michael D&apos;Angelo <Link to="/">mldangelo.com</Link>.</p>
+      <p className="copyright">
+        &copy; Elvan Ceyhan <Link to="/">Elvan Ceyhan.com</Link>.
+      </p>
     </section>
   </section>
 );
